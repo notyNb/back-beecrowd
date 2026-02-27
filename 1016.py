fab163b4ba541ee267f55bd376660978ed90dd52
unknown <pedrinhoyone@gmail.com>
2026-02-27 16:25:28 -0400

    1016 beecrowd

---
+++ 1016.py
@@ -0,0 +1,22 @@
+def main():
+    n = int(input())
+    x = 60
+    y = 90
+
+    a = 0 
+    b = 0
+    tempo = 0
+    while n != (b-a):
+        a += 1
+        tempo = tempo+1
+        b += 1.5
+        
+        
+    print(f"{tempo} minutos")
+
+main()
+
+
+
+
+    
