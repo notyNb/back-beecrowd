c8de58719630e34ac0a335d7246fa58413a10570
unknown <pedrinhoyone@gmail.com>
2026-03-02 18:11:28 -0400

    otimização

--- 1253.py
+++ 1253.py
@@ -2,15 +2,14 @@ def cesar(alfaOriginal:list, lista:list, quantidade:int):
     listaNova = []
 
     for i in range(len(lista)):
-        j = i
+        j = 0
         while lista[i] != alfaOriginal[j]:
             j += 1
-            if j > 25:
-                j = 0
+            
         
         novo_indice = (j - quantidade) % 26
-
         listaNova.append(alfaOriginal[novo_indice])
+
     print("".join(listaNova))
 
 
@@ -21,7 +20,10 @@ def cesar(alfaOriginal:list, lista:list, quantidade:int):
 
 def main():
     n = int(input())
-    alfaOriginal = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z"]
+    alfaOriginal = ["A", "B", "C", "D", "E", "F", "G",
+                    "H", "I", "J", "K", "L", "M", "N",
+                    "O", "P", "Q", "R", "S","T", "U",
+                    "V", "W", "X", "Y", "Z"]
     for i in range(n):
         alfa = input()
         lista = list(alfa)
